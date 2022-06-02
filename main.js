ebd670f75e3e96d87a90cedf5dce12f64f5d2900
Franz Gatt <franz.gatt@student.uibk.ac.at>
2022-06-02 17:37:26 +0200

    Zeitpunkt für Wettervorhersage in den Popup-Titel

--- main.js
+++ main.js
@@ -93,7 +93,13 @@ async function loadWeather(url) {
     let details = jsondata.properties.timeseries[0].data.instant.details;
     // console.log("Aktuelle Wetterdaten", details);
 
+    let forecastDate = new Date(jsondata.properties.timeseries[0].time);
+    // console.log(forecastDate);
+    let forecastLabel = formatDate(forecastDate);
+    // console.log(forecastLabel);
+
     let popup = `
+    <strong>Wettervorhersage für ${forecastLabel}</strong>
     <ul>
     <li>Luftdruck: ${details.air_pressure_at_sea_level} (hPa)</li>
     <li>Lufttemperatur: ${details.air_temperature} (°C)</li>
